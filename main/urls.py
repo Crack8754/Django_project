5a3d66dc5bc8dc72a0ea4911b9a6a574a73fe5f0
Crack8754 <puyadmitriy98@gmail.com>
2026-03-23 22:53:06 +0200

    update

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import home, about
+from . import views
 
 urlpatterns = [
    path('', home, name='home'),
    path('about/', about, name='about'),
+   path('register/', views.register, name='register'),
+   path('login/', views.user_login, name='login'),
+   path('create/', views.create_article, name='create'),
+   path('articles/', views.articles, name='articles'),
 ]
+
 
--- main/views.py
+++ main/views.py
@@ -1,5 +1,50 @@
 from django.shortcuts import render
+from .models import Article
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from .forms import ArticleForm
 
+@login_required
+def create_article(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.save()
+            return redirect('articles')
+    else:
+        form = ArticleForm()
+
+    return render(request, 'main/create.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        user = authenticate(
+            request,
+            username=request.POST['username'],
+            password=request.POST['password']
+        )
+        if user:
+            login(request, user)
+            return redirect('articles')
+
+    return render(request, 'main/login.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        User.objects.create_user(username=username, password=password)
+        return redirect('login')
+
+    return render(request, 'main/register.html')
+
+def articles(request):
+    articles = Article.objects.all()
+    return render(request, 'main/articles.html', {'articles': articles})
 
 def home(request):
    return render(request, 'main/home.html')
